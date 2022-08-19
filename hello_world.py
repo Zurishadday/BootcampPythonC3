f5ea50c2d47c8cae746e95b86b8e7b9c8fa303f9
Zurisadai VM <zurishaddayvm@gmail.com>
2022-08-18 21:05:14 -0500

    comment

--- hello_world.py
+++ hello_world.py
@@ -4,3 +4,7 @@ print("hello world")
 #input()
 
 print("Oye")
+
+
+
+print("Lo que sigue")
---
+++ text.py
@@ -0,0 +1,9 @@
+#x= 1+2 
+#x = int("1")+int("2")
+#x = "hola mundo".capitalize()
+#x = "10".isdigit()
+
+x = 12/5
+
+print(type(x))
+#print(int("10"))
